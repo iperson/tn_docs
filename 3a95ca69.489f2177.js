9aa19a86cd4df579511b2cad8a5e635aee4349bf
Val <gat987@gmail.com>
2021-07-21 18:25:35 -0400

    Deploy website - based on a63eedad5f30322edb51fff774d8f79c96ed654e

---
+++ 3a95ca69.489f2177.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{76:function(o){o.exports=JSON.parse('{"permalink":"/tn_docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
